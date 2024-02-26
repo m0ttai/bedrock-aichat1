6f1405e80f70c5b2d45b761a6d797ca5b0ffec62
y1.mashimo <y1.mashimo@gmail.com>
2024-02-26 11:08:36 +0000

    init

---
+++ developments/requirements.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# get-pip.pyスクリプトをダウンロードして、Pythonで実行する
+curl -kL https://bootstrap.pypa.io/get-pip.py | python
+
+# Pythonのpipモジュールを使用して、複数のパッケージを一括でインストールする
+python -m pip install boto3 streamlit streamlit_chat langchain-community bs4 PyPDF2
